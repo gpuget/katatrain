718cf6f8b335030ab853b79d302a98e8780b6962
Gaëtan Puget <gpuget@excilys.com>
2020-11-24 19:53:27 +0100

    TEST Acceptances

---
+++ src/test/java/com/excilys/katatrain/domain/services/TicketServiceTest.java
@@ -0,0 +1,32 @@
+package com.excilys.katatrain.domain.services;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class TicketServiceTest {
+    @Test
+    public void should_reserve_seats_when_unreserved_seats_are_available() {
+        fail("Not implemented yet.");
+    }
+
+    @Test
+    public void should_not_reserve_when_train_is_full() {
+        fail("Not implemented yet.");
+    }
+
+    @Test
+    public void should_not_reserve_when_train_exceeds_the_limit() {
+        fail("Not implemented yet.");
+    }
+
+    @Test
+    public void should_retrieve_booking_reference_to_reserve() {
+        fail("Not implemented yet.");
+    }
+
+    @Test
+    public void should_mark_seats_as_reserved_once_reserved() {
+        fail("Not implemented yet.");
+    }
+}
