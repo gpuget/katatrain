7e8961f8264e593c953cb611f532defd9ed822fb
Gaëtan Puget <gpuget@excilys.com>
2020-11-24 16:28:38 +0100

    Init commit

---
+++ src/main/java/com/excilys/katatrain/KataTrainApplication.java
@@ -0,0 +1,13 @@
+package com.excilys.katatrain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KataTrainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KataTrainApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/excilys/katatrain/KataTrainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.excilys.katatrain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KataTrainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
