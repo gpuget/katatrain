d9395633df50249df99e1b83f01c6ab5dcb338a1
Gaëtan Puget <gpuget@excilys.com>
2020-11-25 14:54:36 +0100

    REFACTO TEST Fail not enough seats

--- src/test/java/com/excilys/katatrain/domain/services/TicketServiceTest.java
+++ src/test/java/com/excilys/katatrain/domain/services/TicketServiceTest.java
@@ -5,6 +5,7 @@ import com.excilys.katatrain.domain.core.Reservation;
 import com.excilys.katatrain.domain.core.Seat;
 import com.excilys.katatrain.domain.core.TrainSnapshot;
 import com.excilys.katatrain.domain.ports.TrainDataProvider;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -12,6 +13,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Collection;
+import java.util.Collections;
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -28,17 +30,15 @@ public class TicketServiceTest {
     @InjectMocks
     private TicketService ticketService;
 
-    private static TrainSnapshot emptyTrainSnapshot(int numberOfSeats) {
-        return trainSnapshot(numberOfSeats, 0);
-    }
-
     private static TrainSnapshot trainSnapshot(int numberOfSeats, int numberOfReserved) {
         Set<Seat> reservedSeats = IntStream.rangeClosed(1, numberOfReserved)
                 .mapToObj(i -> Seat.reserved(i, "A", BookingReference.of("XCLSDDD")))
                 .collect(Collectors.toSet());
-        Set<Seat> unreservedSeats = IntStream.rangeClosed(numberOfReserved + 1, numberOfSeats)
+        Set<Seat> unreservedSeats = (numberOfReserved < numberOfSeats)
+                ? IntStream.rangeClosed(numberOfReserved + 1, numberOfSeats)
                 .mapToObj(i -> Seat.unreserved(i, "A"))
-                .collect(Collectors.toSet());
+                .collect(Collectors.toSet())
+                : Collections.emptySet();
         return TrainSnapshot.from(Stream.of(reservedSeats, unreservedSeats)
                 .flatMap(Collection::stream)
                 .collect(Collectors.toSet()));
@@ -62,20 +62,66 @@ public class TicketServiceTest {
 
     @Test
     public void should_not_reserve_when_train_is_full() {
-        fail("Not implemented yet.");
+        // GIVEN
+        String trainId = "TGV2611";
+        int numberOfSeats = 2;
+        TrainSnapshot trainSnapshot = trainSnapshot(10, 10);
+        when(this.trainDataProvider.getTrain(trainId)).thenReturn(trainSnapshot);
+
+        // WHEN
+        Reservation reservation = this.ticketService.reserve(numberOfSeats, trainId);
+
+        // THEN
+        assertThat(reservation).isNotNull();
+        assertThat(reservation.numberOfSeats()).isZero();
     }
 
     @Test
+    public void should_not_reserve_when_not_enough_available_seats() {
+        // GIVEN
+        String trainId = "TGV2611";
+        int numberOfSeats = 2;
+        TrainSnapshot trainSnapshot = trainSnapshot(1, 0);
+        when(this.trainDataProvider.getTrain(trainId)).thenReturn(trainSnapshot);
+
+        // WHEN
+        Reservation reservation = this.ticketService.reserve(numberOfSeats, trainId);
+
+        // THEN
+        assertThat(reservation).isNotNull();
+        assertThat(reservation.numberOfSeats()).isZero();
+    }
+
+    @Test
+    public void should_not_reserve_when_not_enough_unreserved_seats() {
+        // GIVEN
+        String trainId = "TGV2611";
+        int numberOfSeats = 2;
+        TrainSnapshot trainSnapshot = trainSnapshot(10, 8);
+        when(this.trainDataProvider.getTrain(trainId)).thenReturn(trainSnapshot);
+
+        // WHEN
+        Reservation reservation = this.ticketService.reserve(numberOfSeats, trainId);
+
+        // THEN
+        assertThat(reservation).isNotNull();
+        assertThat(reservation.numberOfSeats()).isZero();
+    }
+
+    @Test
+    @Disabled
     public void should_not_reserve_when_train_exceeds_the_limit() {
         fail("Not implemented yet.");
     }
 
     @Test
+    @Disabled
     public void should_retrieve_booking_reference_to_reserve() {
         fail("Not implemented yet.");
     }
 
     @Test
+    @Disabled
     public void should_mark_seats_as_reserved_once_reserved() {
         fail("Not implemented yet.");
     }
